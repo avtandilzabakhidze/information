72a0e0e5db91a9715cecc56be2200b295c4bb17a
Revaz Vazagashvili <r.vazagashvili@ssu.edu.ge>
2024-11-04 22:55:58 +0400

    dropdown list

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const navbarToggler = document.querySelector('.navbar-toggler');
 const navbarBrand = document.getElementById('navbarBrand');
 
+
 navbarToggler.addEventListener('click', () => {
   if (navbarBrand.style.display === 'none') {
-    setTimeout(() => {
+    
       navbarBrand.style.display = 'block';
-    }, 400);
-  } else {
+  }
+   else {
     navbarBrand.style.display = 'none';
   }
 });
