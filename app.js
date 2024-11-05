4bd50a3d87ec32da6c81ebf6c003a5db882ceafd
Revaz Vazagashvili <r.vazagashvili@ssu.edu.ge>
2024-11-05 22:18:58 +0400

    11/5/2024

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// const navbarToggler = document.querySelector('.navbar-toggler');
+// const navbarBrand = document.getElementById('navbarBrand');
+
+
+// navbarToggler.addEventListener('click', () => {
+//   if (navbarBrand.style.display === 'none') {
+    
+//       navbarBrand.style.display = 'block';
+//   }
+//    else {
+//     navbarBrand.style.display = 'none';
+//   }
+// });
+
+
 const navbarToggler = document.querySelector('.navbar-toggler');
 const navbarBrand = document.getElementById('navbarBrand');
 
-
 navbarToggler.addEventListener('click', () => {
   if (navbarBrand.style.display === 'none') {
+    setTimeout(() => {
     
       navbarBrand.style.display = 'block';
+    }, 400);
   }
    else {
     navbarBrand.style.display = 'none';
