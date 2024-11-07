c67a5917543f4604bb0ef073897b47a9d8224b38
avtandilzabakhidze <zabakhidze01@gmail.com>
2024-11-07 22:01:43 +0400

    fix nav bar

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-// const navbarToggler = document.querySelector('.navbar-toggler');
-// const navbarBrand = document.getElementById('navbarBrand');
-
-
-// navbarToggler.addEventListener('click', () => {
-//   if (navbarBrand.style.display === 'none') {
-    
-//       navbarBrand.style.display = 'block';
-//   }
-//    else {
-//     navbarBrand.style.display = 'none';
-//   }
-// });
-
-
 const navbarToggler = document.querySelector('.navbar-toggler');
 const navbarBrand = document.getElementById('navbarBrand');
 
 navbarToggler.addEventListener('click', () => {
   if (navbarBrand.style.display === 'none') {
     setTimeout(() => {
-    
-      navbarBrand.style.display = 'block';
+       navbarBrand.style.display = 'block';
     }, 400);
   }
    else {
