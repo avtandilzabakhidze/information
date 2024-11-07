df2d2853418b7f5ff52c29fe5d5b6e0f3200bbff
Revaz Vazagashvili <r.vazagashvili@ssu.edu.ge>
2024-11-08 01:08:18 +0400

    Know + Form

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const navbarToggler = document.querySelector('.navbar-toggler');
 const navbarBrand = document.getElementById('navbarBrand');
 
+// Function to toggle the visibility of #navbarBrand
 navbarToggler.addEventListener('click', () => {
-  if (navbarBrand.style.display === 'none') {
-    setTimeout(() => {
-       navbarBrand.style.display = 'block';
-    }, 400);
-  }
-   else {
-    navbarBrand.style.display = 'none';
+  const screenWidth = window.innerWidth;
+  
+  // Check if we are on mobile screen
+  if (screenWidth < 768) {
+    // Toggle the display on mobile screens
+    navbarBrand.classList.toggle('d-block');  // Show or hide using d-block
+    navbarBrand.classList.toggle('d-none');   // Ensure it's hidden on tablet/PC screens
   }
 });
