70793c1df885bffa4a0bc7df0759919a69ca0b1a
avtandilzabakhidze <zabakhidze01@gmail.com>
2024-11-08 20:44:47 +0400

    fix bugs

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const navbarToggler = document.querySelector('.navbar-toggler');
 const navbarBrand = document.getElementById('navbarBrand');
 
-// Function to toggle the visibility of #navbarBrand
 navbarToggler.addEventListener('click', () => {
   const screenWidth = window.innerWidth;
-  
-  // Check if we are on mobile screen
-  if (screenWidth < 768) {
-    // Toggle the display on mobile screens
-    navbarBrand.classList.toggle('d-block');  // Show or hide using d-block
-    navbarBrand.classList.toggle('d-none');   // Ensure it's hidden on tablet/PC screens
+
+  if (screenWidth < 2000) {
+    navbarBrand.classList.toggle('d-block');
+    navbarBrand.classList.toggle('d-none');
   }
 });
