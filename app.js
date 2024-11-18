7d183f029ae75674cb337e02fd090fab565e1f51
Revaz Vazagashvili <r.vazagashvili@ssu.edu.ge>
2024-11-18 21:49:10 +0400

    gifs

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ navbarToggler.addEventListener('click', () => {
 // test
 let score = 0;
 
-let scoreCounter = 0;
-
 function checkAnswer(element, isCorrect, nextTestId) {
-  scoreCounter++;
   if (isCorrect) {
     element.style.border = "2px solid green";
     score++;
@@ -30,10 +27,14 @@ function checkAnswer(element, isCorrect, nextTestId) {
       const nextTestElement = document.getElementById(nextTestId);
       nextTestElement.style.display = 'block';
 
-      if (nextTestId === 'test-resut') {
+      if (nextTestId === 'test-resut' && score <= 1) {
         nextTestElement.innerHTML += ` <div class="container" style="text-align: center;"><img class="container" src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHdjZ3ViY2lscW9ldHY0Y2pxYjR3N2N3ZmY2ZGU1OHdqZmx3bzhvayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UShW8A2CRzW67yU4qp/giphy.webp" 
         style="max-width: 1000px; max-height: 700px; display: block; margin: 0 auto;"><p class = "container" style = "text-aling: center !important; padding: 20px 0; margin: 0 auto !important;">წარმატებული დასასრული , შედეგი: ${score}</p> </div>`;
-      }
+       } else if (nextTestId === 'test-resut' && score <= 2)
+       {
+         nextTestElement.innerHTML += ` <div class="container" style="text-align: center;"><img class="container" src="https://media3.giphy.com/media/l41m5h1yLXqAi5Uf6/giphy.gif?cid=6c09b952fk9caxrlrikdmhba0ux72fi7mppj77wbl96x4qeg&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g" 
+         style="max-width: 1000px; max-height: 700px; display: block; margin: 0 auto;"><p class = "container" style = "text-aling: center !important; padding: 20px 0; margin: 0 auto !important;">წარმატებული დასასრული , შედეგი: ${score}</p> </div>`;
+       }
     }, 1000);
   }
 
